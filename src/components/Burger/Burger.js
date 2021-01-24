0f810ecd7e753c5ed5c97e86d6a6b878e7d7ab08
ANgajasinghe <Akalanka Nayanajith@DESKTOP-TERE1H0>
2021-01-24 16:26:07 +0530

    13. Work with reducer() => For transform array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,27 +3,30 @@ import './Burger.css';
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const burger = (props) => {
-
-    // Object to array =  Object.keys()
-    console.log(props.ingredients);
     const objetKeyArray = Object.keys(props.ingredients); // ["salad", "bacon", "cheese", "meat"]
-    const transformedIngredients = objetKeyArray
-        .map(igKey =>{
+    let transformedIngredients = objetKeyArray
+        .map(igKey => {
             // Create array Array(LENGTH)
-            console.log(igKey);
             return [...Array(props.ingredients[igKey])]
-                .map((_,i) => {
-                return <BurgerIngredient key={igKey + i} type={igKey}/>
-            })
-        })
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey}/>
+                })
+        }).reduce((arr, el) => {
+            // this reduce methods allow us to transform array to another format.
+            return arr.concat(el);
+        },[])
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p> Please start adding ingredients</p>
+    }
 
 
-    return(
-      <div className='Burger'>
-          <BurgerIngredient type='bread-top'/>
-          {transformedIngredients}
-          <BurgerIngredient type='bread-bottom'/>
-      </div>
+    return (
+        <div className='Burger'>
+            <BurgerIngredient type='bread-top'/>
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom'/>
+        </div>
     );
 }
 export default burger;
